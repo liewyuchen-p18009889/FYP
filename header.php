712c427773cfc17735fb0a0c4faec4518fb52c17
liewyuchen <uchen@globalconnect.com.my>
2021-08-06 16:16:09 +0800

    init

--- header.php
+++ header.php
@@ -1,26 +1,40 @@
-<div class="container-fluid" style="background: #1f52a3;" id="header">
+<div class="container-fluid" style="background: #3AAFA9;" id="header">
 	<div class="row">
 		<div class="col-2"></div>
 		<div class="col-8">
-			<a href="/205CDE/Assignment/home.php"><h1 style="text-align: center; color: #e6e8eb; margin: 20px 0;">U Chen Daily</h1></a>
+			<a href="#"><h1 style="text-align: center; color: #feffff; margin: 20px 0;">UTask</h1></a>
 		</div>
 		<div class="col-2 d-flex justify-content-center align-items-center">
-			<?php if(isset($_SESSION['username'])){ ?>
-				<div class="dropdown">
-					<a class="btn btn-outline-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						<i class="fas fa-user"></i>
-					</a>
-					<div class="dropdown-menu shadow bg-white rounded" aria-labelledby="dropdownMenuLink">
-						<a class="dropdown-item" href="/205CDE/Assignment/manageNews.php" target="_blank"><i class="fas fa-user-cog" style="color: #1f52a3"></i>&nbsp;Manage News</a>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="/205CDE/Assignment/home.php?logout='1'"><i class="fas fa-sign-out-alt" style="color: #1f52a3"></i>&nbsp;Logout</a>
-					</div>
-				</div>
-				<?php }else{ 
-				?>
-				<button type="button" class="btn btn-outline-light" data-toggle="modal" data-target="#loginFormModal">LOGIN</button>
-			<?php } ?>
-			<!--<a href="/205CDE/Assignment/manageNews.php" target="_blank" class="btn btn-outline-light">LOGIN</a>-->
 		</div>
 	</div>
 </div>
+<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<a class="navbar-brand" href="/205CDE/Assignment/home.php"><i class="fas fa-home" style="font-size: 30px; color: #1f52a3;"></i></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button> -->
+	
+	<!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item" style="margin: 0 15px;">
+				<a class="nav-link" href="/205CDE/Assignment/newsList.php" target="_blank">All News</a>
+			</li>
+			<li class="nav-item" style="margin: 0 15px;">
+				<a class="nav-link" href="/205CDE/Assignment/aboutUs.php" target="_blank">About Us</a>
+			</li>
+			<li class="nav-item" style="margin: 0 15px;">
+				<a class="nav-link" href="/205CDE/Assignment/contactUs.php" target="_blank">Contact Us</a>
+			</li>
+			<li class="nav-item" style="margin: 0 15px;">
+				<a class="nav-link" href="/205CDE/Assignment/faqs.php" target="_blank">FAQs</a>
+			</li> -->
+			<!-- <li class="nav-item"> -->
+			<!-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
+			<!-- </li> -->
+		<!-- </ul> -->
+		<!-- <form class="form-inline my-2 my-lg-0"> -->
+		<!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+		<!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+		<!-- </form> -->
+	<!-- </div> -->
+<!-- </nav> -->
