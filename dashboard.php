8c5e95ba8851cec21a04842e20409b1635197336
liewyuchen <uchen@globalconnect.com.my>
2021-10-13 16:12:26 +0800

    update dashboard

--- dashboard.php
+++ dashboard.php
@@ -16,15 +16,29 @@
 </head>
 
 <body class="bg-light">
-    <?php include 'header.php'; ?>
+    <?php include 'header.php'; 
+    
+        // check if user is the project's member so that can access to the project's dashboard START
+        $query1 = "SELECT * FROM project_members WHERE user_id={$_SESSION['user_id']} AND project_id={$_GET['id']}";
+        $runQuery1 = mysqli_query($dbc, $query1);
+        $countQuery1 = mysqli_num_rows($runQuery1);
+        
+        if($countQuery1 == 0){
+            echo '<script language="javascript">
+                    alert("Invalid URL!");
+                    window.location.href="/FYP/projectList.php";
+                    </script>';
+        }
+        // check if user is the project member so that can access to the project's dashboard END
+    ?>
     <div class="container-fluid" style="padding: 30px 10px;">
         <div class="row" style="margin: 0 35px;">
             <div class="col-6 pl-0">
                 <h3 class="text-info"><span style="cursor:pointer" onclick="openNav()">&#9776;</span> Dashboard</h3>
             </div>
             <div class="col-6 d-flex justify-content-end">
-                <button class="btn btn-info" type="button" data-toggle="modal" data-target="#addTaskModal"><i
-                        class="fas fa-file-pdf" style="font-size: 20px;"></i>&nbsp;Generate Report</button>
+                <button class="btn btn-info" type="button"><i class="fas fa-file-pdf"
+                        style="font-size: 20px;"></i>&nbsp;Generate Report</button>
             </div>
             <!-- <div class="col-6 d-flex justify-content-end"></div> -->
             <!-- <div class="col-6 d-flex justify-content-end">
@@ -36,49 +50,106 @@
     </div>
     <div class="container-fluid mb-4">
         <div class="card-deck ml-3 mr-3 mb-5">
-            <div class="card shadow border-success">
-                <div class="card-body">
-                    <!-- <h5 class="card-title text-info">Total Tasks</h5> -->
-                    <!-- <h1 class="card-text text-center"><i class="fas fa-tachometer-alt"></i></h1> -->
-                    <h2 class="card-text text-center">Total tasks</h2>
-                    <h1 class="card-text text-center text-success"><span class="counter">188</span></h1>
-                </div>
-            </div>
-            <div class="card shadow border-primary">
-                <div class="card-body">
-                    <h2 class="card-text text-center">To do tasks</h2>
-                    <h1 class="card-text text-center text-primary"><span class="counter">6</span></h1>
-                </div>
-            </div>
+            <!-- total tasks START -->
             <div class="card shadow border-info">
                 <div class="card-body">
-                    <h2 class="card-text text-center">In progress tasks</h2>
-                    <h1 class="card-text text-center text-info"><span class="counter">12</span></h1>
+                    <h2 class="card-text text-center">Total</h2>
+                    <h1 class="card-text text-center text-info"><span class="counter">
+                            <?php 
+                            $query2 = "SELECT * FROM tasks WHERE task_project='{$_GET['id']}'";
+                            $runQuery2 = mysqli_query($dbc, $query2);
+                            $countTotal = mysqli_num_rows($runQuery2);
+
+                            echo $countTotal; 
+                        ?>
+                        </span></h1>
                 </div>
             </div>
-            <!-- <div class="card shadow border-warning">
+            <!-- total tasks END -->
+            <!-- in progress tasks START -->
+            <div class="card shadow border-primary">
                 <div class="card-body">
-                <h2 class="card-text text-center">Overdue tasks</h2>
-                    <h1 class="card-text text-center text-info">6</h1>
+                    <h2 class="card-text text-center">In progress</h2>
+                    <h1 class="card-text text-center text-primary"><span class="counter">
+                            <?php 
+                            $query3 = "SELECT * FROM tasks WHERE task_project='{$_GET['id']}' AND task_status='In Progress'";
+                            $runQuery3 = mysqli_query($dbc, $query3);
+                            $countInProgress = mysqli_num_rows($runQuery3);
+
+                            echo $countInProgress;
+                        ?>
+                        </span></h1>
                 </div>
-            </div> -->
+            </div>
+            <!-- in progress tasks END -->
+            <!-- completed tasks START -->
+            <div class="card shadow border-success">
+                <div class="card-body">
+                    <h2 class="card-text text-center">Completed</h2>
+                    <h1 class="card-text text-center text-success"><span class="counter">
+                            <?php 
+                            $query4 = "SELECT * FROM tasks WHERE task_project='{$_GET['id']}' AND task_status='Done'";
+                            $runQuery4 = mysqli_query($dbc, $query4);
+                            $countDone = mysqli_num_rows($runQuery4);
+
+                            echo $countDone;
+                        ?>
+                        </span></h1>
+                </div>
+            </div>
+            <!-- completed tasks END -->
+            <!-- overdue tasks START -->
+            <div class="card shadow border-danger">
+                <div class="card-body">
+                    <h2 class="card-text text-center">Overdue</h2>
+                    <h1 class="card-text text-center text-danger"><span class="counter">
+                            <?php 
+                            $query5 = "SELECT * FROM tasks WHERE task_project='{$_GET['id']}' AND task_status!='Done'";
+                            $runQuery5 = mysqli_query($dbc, $query5);
+                            $todayDate = date("Y-m-d");
+                            $countOverdue = 0;
+                            
+                            foreach($runQuery5 as $row5){
+                                if($todayDate > $row5['task_end']){
+                                    $countOverdue++;
+                                }
+                            }
+                            echo $countOverdue;
+                        ?>
+                        </span></h1>
+                </div>
+            </div>
+            <!-- overdue tasks END -->
         </div>
-        <!-- <div class="row d-flex justify-content-center">
-            <div class="col-3 shadow p-4 mb-5 mr-3 bg-white rounded"></div>
-            <div class="col-3 shadow p-4 mb-5 bg-white rounded"></div>
-            <div class="col-5 shadow p-4 mb-5 ml-3 bg-white rounded"></div>
-        </div> -->
         <div class="row d-flex justify-content-center">
             <!-- <div class="col-3 shadow p-4 mb-5 mr-3 bg-white rounded"></div>
             <div class="col-3 shadow p-4 mb-5 bg-white rounded"></div>
             <div class="col-5 shadow p-4 mb-5 ml-3 bg-white rounded"><h5 class="text-info">Activity</h5></div> -->
 
+            <?php 
+                $query5 = "SELECT * FROM tasks WHERE task_project='{$_GET['id']}' AND task_status='To Do'";
+                $runQuery5 = mysqli_query($dbc, $query5);
+                $countToDo = mysqli_num_rows($runQuery5);
+
+                $query6 = "SELECT * FROM tasks WHERE task_project='{$_GET['id']}' AND task_status='Test'";
+                $runQuery6 = mysqli_query($dbc, $query6);
+                $countTest = mysqli_num_rows($runQuery6);
+            ?>
+
+            <!-- pass value to chart.js START -->
+            <input type="hidden" id="count_toDo" value="<?php echo $countToDo; ?>">
+            <input type="hidden" id="count_inProgress" value="<?php echo $countInProgress; ?>">
+            <input type="hidden" id="count_test" value="<?php echo $countTest; ?>">
+            <input type="hidden" id="count_done" value="<?php echo $countDone; ?>">
+            <input type="hidden" id="count_overdue" value="<?php echo $countOverdue; ?>">
+            <!-- pass value to chart.js END -->
+
             <div class="col-7 shadow p-4 mb-5 mr-3 ml-0 bg-white rounded">
-                <h5 class="text-info">Bar</h5>
+                <h5 class="text-info">Project Status</h5>
                 <canvas id="myChart" width="100" height="55"></canvas>
             </div>
             <div class="col-4 shadow p-4 mb-5 ml-3 mr-0 bg-white rounded">
-                <h5 class="text-info">Pie</h5>
+                <h5 class="text-info">Project Status</h5>
                 <canvas id="myChart2" width="" height=""></canvas>
             </div>
         </div>
@@ -87,29 +158,30 @@
     <?php include 'footer.html'; ?>
     <!-- bar chart START -->
     <script>
+        var countToDo = $('#count_toDo').val();
+        var countInProgress = $('#count_inProgress').val();
+        var countTest = $('#count_test').val();
+        var countDone = $('#count_done').val();
+        var countOverdue = $('#count_overdue').val();
+
         var ctx = document.getElementById('myChart');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: ['To Do', 'In Progress', 'Test', 'Done'],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: [countToDo, countInProgress, countTest, countDone],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(75, 192, 192, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(75, 192, 192, 1)'
                     ],
                     borderWidth: 1
                 }]
@@ -119,6 +191,11 @@
                     y: {
                         beginAtZero: true
                     }
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    }
                 }
             }
         });
@@ -127,17 +204,18 @@
     <!-- pie chart START -->
     <script>
         var ctx2 = document.getElementById('myChart2');
+
         var myChart2 = new Chart(ctx2, {
             type: 'pie',
             data: {
                 labels: [
-                    'Red',
-                    'Blue',
-                    'Yellow'
+                    'In Progress',
+                    'Completed',
+                    'Overdue'
                 ],
                 datasets: [{
                     label: 'My First Dataset',
-                    data: [300, 50, 100],
+                    data: [countInProgress, countDone, countOverdue],
                     backgroundColor: [
                         'rgb(255, 99, 132)',
                         'rgb(54, 162, 235)',
--- sideMenuBar.php
+++ sideMenuBar.php
@@ -1,14 +1,15 @@
 <div id="mySidenav" class="sidenav shadow-lg">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href="/FYP/projectList.php">View All Projects</a>
+    <a href="/FYP/projectList.php">My Projects</a>
     <div class="dropdown-divider"></div>
     <a href="/FYP/dashboard.php?id=<?php echo $_GET['id'] ?>">Dashboard</a>
     <a href="/FYP/taskBoard.php?id=<?php echo $_GET['id'] ?>">Task Board</a>
     <a href="#">Calendar</a>
+    <a href="#">Manage Members</a>
 </div>
 <script>
     function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.width = "280px";
     }
 
     function closeNav() {
